2cda153c046491462a05f610da67e9718b97f56b
Brian Schubert <schubert.b@northeastern.edu>
2024-08-25 21:09:28 -0400

    Implement frequency converter construction, scale lookup, accessors

--- src/spectral_unit_convert/_freq.py
+++ src/spectral_unit_convert/_freq.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import abc
-from typing import TYPE_CHECKING, Final, Generic, Literal, TypeVar, Union
+from typing import TYPE_CHECKING, Any, ClassVar, Final, Generic, Literal, TypeVar, Union
 
 if TYPE_CHECKING:
     from typing_extensions import TypeAlias
@@ -75,10 +75,47 @@ _PREFIX_SCALES: Final[dict[_UNIT_PREFIX, int]] = {
 
 
 class _FrequencyMeasure(Generic[_T], abc.ABC):
+    # Value expressed in base units is equal to `_value * 10 ** _scale`
     _value: _T
     _scale: int
 
+    _SCALES: ClassVar[
+        dict[_UNIT_FREQUENCY, int]
+        | dict[_UNIT_WAVELENGTH, int]
+        | dict[_UNIT_WAVENUMBER, int]
+    ]
+
     __slots__ = ("_value", "_scale")
+
+    def __init_subclass__(
+        cls, *, unit_suffix: str, invert: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init_subclass__(**kwargs)
+
+        sign = -1 if invert else 1
+        cls._SCALES = {  # type: ignore[assignment]
+            f"{prefix}{unit_suffix}": sign * scale
+            for prefix, scale in _PREFIX_SCALES.items()
+        }
+
+    def __repr__(self) -> str:
+        return f"{type(self).__qualname__}({self.value!r}, {self.unit!r})"
+
+    @property
+    def value(self) -> _T:
+        return self._value
+
+    @property
+    def scale(self) -> int:
+        return self._scale
+
+    @property
+    def unit(self) -> _UNIT_FREQUENCY | _UNIT_WAVELENGTH | _UNIT_WAVENUMBER:
+        return next(
+            iter(
+                symbol for symbol, scale in self._SCALES.items() if scale == self._scale
+            )
+        )
 
     @abc.abstractmethod
     def as_frequency(self, unit: _UNIT_FREQUENCY) -> Frequency[_T]: ...
@@ -90,13 +127,31 @@ class _FrequencyMeasure(Generic[_T], abc.ABC):
     def as_wavenumber(self, unit: _UNIT_WAVENUMBER) -> Wavenumber[_T]: ...
 
 
-class Frequency(_FrequencyMeasure[_T]):
+class Frequency(_FrequencyMeasure[_T], unit_suffix="Hz"):
     __slots__ = ()
 
+    _SCALES: ClassVar[dict[_UNIT_FREQUENCY, int]]
 
-class Wavelength(_FrequencyMeasure[_T]):
+    def __init__(self, value: _T, unit: _UNIT_FREQUENCY) -> None:
+        self._value = value
+        self._scale = self._SCALES[unit]
+
+
+class Wavelength(_FrequencyMeasure[_T], unit_suffix="m"):
     __slots__ = ()
 
+    _SCALES: ClassVar[dict[_UNIT_WAVELENGTH, int]]
 
-class Wavenumber(_FrequencyMeasure[_T]):
+    def __init__(self, value: _T, unit: _UNIT_WAVELENGTH) -> None:
+        self._value = value
+        self._scale = self._SCALES[unit]
+
+
+class Wavenumber(_FrequencyMeasure[_T], unit_suffix="m-1", invert=True):
     __slots__ = ()
+
+    _SCALES: ClassVar[dict[_UNIT_WAVENUMBER, int]]
+
+    def __init__(self, value: _T, unit: _UNIT_WAVENUMBER) -> None:
+        self._value = value
+        self._scale = self._SCALES[unit]
