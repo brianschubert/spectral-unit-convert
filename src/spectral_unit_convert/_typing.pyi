05cfe184212223af03e2e2c18e0c94c1cda4ad23
Brian Schubert <schubert.b@northeastern.edu>
2024-08-26 20:32:46 -0400

    Convert _typing to stub file

--- src/spectral_unit_convert/_typing.pyi
+++ src/spectral_unit_convert/_typing.pyi
@@ -1,16 +1,9 @@
-from __future__ import annotations
+from typing import Protocol
 
-from typing import TYPE_CHECKING, Protocol
-
-if TYPE_CHECKING:
-    from typing_extensions import Self
-
+from typing_extensions import Self
 
 class SupportsFloatDivMul(Protocol):
     def __truediv__(self, other: float | Self) -> Self: ...
-
     def __rtruediv__(self, other: float | Self) -> Self: ...
-
     def __mul__(self, other: float | Self) -> Self: ...
-
     def __rmul__(self, other: float | Self) -> Self: ...
